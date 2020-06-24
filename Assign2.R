a903cc5fe5d914a2d81b8c13aea074b5ae4d2416
argus44 <rkebede0@gmail.com>
2020-06-23 22:17:38 -0600

    Assignment 2

---
+++ Assign2.R
@@ -0,0 +1,28 @@
+makenewCacheMatrix <- function(x = matrix()) {
+  i <- NULL
+  set <- function(y) {
+    x <<- y
+    i <<- NULL
+  }
+  get <- function() x
+  setinverse <- function(inverse) i <<- inverse
+  getinverse <- function() i
+  list(set = set,
+       get = get,
+       setinverse = setinverse,
+       getinverse = getinverse)
+}
+
+## then the function cacheSolveNew would retrive the inverse from cache
+
+cacheSolveNew <- function(x, ...) {
+  i <- x$getinverse()
+  if (!is.null(i)) {
+    message("getting cached data")
+    return(i)
+  }
+  data <- x$get()
+  i <- solve(data, ...)
+  x$setinverse(i)
+  i
+}
